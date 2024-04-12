8b6fc706e413f3453d1723dd6e36da45cb986d5c
Nidhi-bhamare <bhamarenidhi98@gmail.com>
2024-04-12 20:51:31 +0530

    First git  post

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import classes from './MainNavigation.module.css';
+
+function MainNavigation() {
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Event  App</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>All Events</Link>
+          </li>
+          <li>
+            <Link href='/new-meetup'>Add New Event</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
